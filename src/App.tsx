02ce2fe816181682ebb8021e996ad4a6fa372f69
HariBhandari07 <bhandarihari07@gmail.com>
2023-11-22 10:18:23 +0545

    Feat: Setup new react native app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ecommerce</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
